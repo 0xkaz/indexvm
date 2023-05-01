5a18dbeda332041c01ce9ca093336d553ed574f6
kaz <kzzz123@protonmail.com>
2023-05-01 22:31:39 +0400

    rename

--- controller/handler_test_query.go
+++ controller/handler_test_query.go
@@ -8,23 +8,25 @@ import (
 	"github.com/ava-labs/indexvm/grpc"
 )
 
-type QueryArgs struct {
+type TestQueryArgs struct {
 	Method  string `json:"method"`
 	Query   string `json:"query"`
 	Nocache bool   `json:"nocache"`
 }
 
-type QueryReply struct {
+type TestQueryReply struct {
 	Message string `json:"message"`
 	Result  string `json:"result"`
 	Err     string `json:"err"`
 }
 
-func (h *Handler) Query(req *http.Request, args *QueryArgs, reply *QueryReply) error {
+func (h *Handler) TestQuery(req *http.Request, args *TestQueryArgs, reply *TestQueryReply) error {
+
 	r, err := grpc.Query(args.Method, args.Query, args.Nocache)
 	if err != nil {
-		log.Printf("Query ERROR: %v", err)
+		log.Printf("TestQuery ERROR: %v", err)
 	}
+	log.Printf("TestQuery: %v", r.Result)
 	reply.Result = r.Result
 	reply.Err = r.Err
 	reply.Message = fmt.Sprintf("query: %v", args.Method)
